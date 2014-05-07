3f1826bc58d720528473349337f4e4df5cb5b0b5
Steve Bauman <steven_bauman_7@hotmail.com>
2014-05-07 09:46:45 -0400

    MaxMind Driver

---
+++ src/Stevebauman/Location/Drivers/MaxMind.php
@@ -0,0 +1,56 @@
+<?php namespace Stevebauman\Location\Drivers;
+
+use GeoIp2\Database\Reader;
+use GeoIp2\WebService\Client;
+
+class MaxMind {
+	
+	private $location = array();
+	protected $config;
+	
+	
+	public function __construct(){
+		global $app;
+		$this->config = $app['config'];
+	}	
+	
+	/**
+	* Sets location array to
+	*
+	* Thanks to Torann: https://github.com/Torann/laravel-4-geoip/blob/master/src/Torann/GeoIP/GeoIP.php
+	* @param string $ip
+   	*/
+	public function set($ip){
+		$settings = $this->config->get('location::drivers.MaxMind.configuration');
+
+		if($settings['web_service']) {
+			$maxmind = new Client($settings['user_id'], $settings['license_key']);
+		}
+		else {
+			$maxmind = new Reader(app_path().'/database/maxmind/GeoLite2-City.mmdb');
+		}
+		
+		$record = $maxmind->city($ip);
+
+		$this->location = array(
+			"ip"			=> $ip,
+			"isoCode" 		=> $record->country->isoCode,
+			"country" 		=> $record->country->name,
+			"city" 			=> $record->city->name,
+			"state" 		=> $record->mostSpecificSubdivision->isoCode,
+			"postal_code" 	=> $record->postal->code,
+			"lat" 			=> $record->location->latitude,
+			"lon" 			=> $record->location->longitude,
+			"default"       => false
+		);
+
+		unset($record);
+
+		return $this->location;
+	}
+
+	
+	public function get(){
+		return $this->location;
+	}
+}
