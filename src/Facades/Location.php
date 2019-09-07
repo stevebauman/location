08f20e9fff47a28f4e705d8d7b4e6ecf0fb4c167
Steve Bauman <steven_bauman@outlook.com>
2019-09-07 14:20:14 -0400

    Added method annotations

--- src/Facades/Location.php
+++ src/Facades/Location.php
@@ -4,6 +4,12 @@ namespace Stevebauman\Location\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Stevebauman\Location\Position|bool get(string $ip = null)
+ * @method static void setDriver(\Stevebauman\Location\Drivers\Driver $driver)
+ *
+ * @see \Stevebauman\Location\Location
+ */
 class Location extends Facade
 {
     /**
