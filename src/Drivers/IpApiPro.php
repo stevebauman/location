14e19b98bc6043936c08a2e5a1aa072592681914
Steve Bauman <steven_bauman@outlook.com>
2019-09-12 13:59:29 -0400

    Added IpApiPro driver and IP API token to configuration
    Closes #63

---
+++ src/Drivers/IpApiPro.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Stevebauman\Location\Drivers;
+
+class IpApiPro extends IpApi
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function url($ip)
+    {
+        $key = config('ip_api.token');
+
+        return "https://pro.ip-api.com/json/{$ip}?key={$key}";
+    }
+}
