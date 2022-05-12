ae506ebc4b59bb46d24d20fea948c3dc27dd0271
IP Location <hello@ipapi.co>
2022-05-12 17:10:42 +0530

    Driver for ipapi.co by Kloudend

---
+++ src/Drivers/Kloudend.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Stevebauman\Location\Drivers;
+
+use Exception;
+use Illuminate\Support\Fluent;
+use Stevebauman\Location\Position;
+
+class Kloudend extends Driver
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function url($ip)
+    {
+        $token = config('location.kloudend.token', '');
+
+        return "https://ipapi.co/{$ip}/?key={$token}";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function hydrate(Position $position, Fluent $location)
+    {
+        $position->countryName = $location->country_name;
+        $position->countryCode = $location->country_code;
+        $position->regionCode = $location->region_code;
+        $position->regionName = $location->region;
+        $position->cityName = $location->city;
+        $position->zipCode = $location->postal;
+        $position->postalCode = $location->postal;
+        $position->latitude = (string) $location->latitude;
+        $position->longitude = (string) $location->longitude;
+        $position->timezone = $location->timezone;
+
+        return $position;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function process($ip)
+    {
+        try {
+            $response = json_decode($this->getUrlContent($this->url($ip)), true);
+
+            return new Fluent($response);
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+}
