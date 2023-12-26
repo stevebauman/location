b27e95e9746176ec72f65258c8812694a079df96
Steve Bauman <steven_bauman@outlook.com>
2023-12-26 08:52:17 -0500

    Throw HTTP exceptions

--- src/Drivers/HttpDriver.php
+++ src/Drivers/HttpDriver.php
@@ -34,7 +34,11 @@ abstract class HttpDriver extends Driver
     public function process(Request $request): Fluent|false
     {
         return rescue(fn () => new Fluent(
-            $this->http()->acceptJson()->get($this->url($request->getIp()))->json()
+            $this->http()
+                ->throw()
+                ->acceptJson()
+                ->get($this->url($request->getIp()))
+                ->json()
         ), false, false);
     }
 
